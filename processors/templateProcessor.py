f5cd6f322879fd5fd327f487337c27671772f08e
Devin Taylor <dntaylor@ucdavis.edu>
2020-01-07 12:53:25 -0600

    reorganize and add a template processor

---
+++ processors/templateProcessor.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+import os
+import sys
+import logging
+import argparse
+import numpy as np
+import uproot
+import uproot_methods
+from coffea import hist, processor, lookup_tools
+from coffea.lumi_tools import lumi_tools
+from coffea.util import load, save
+from coffea.analysis_objects import JaggedCandidateArray
+from awkward import JaggedArray, IndexedArray
+
+ZMASS = 91.1876
+
+logger = logging.getLogger("TemplateProcessor")
+logging.basicConfig(level=logging.INFO, stream=sys.stderr,format='%(asctime)s.%(msecs)03d %(levelname)s %(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+class TemplateProcessor(processor.ProcessorABC):
+    # will sync with
+    # https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsZZ4lRunIILegacy
+    # to the best of NanoAOD ability
+    def __init__(self,year='2018',corrections={}):
+        self._year = year
+
+        self._corrections = corrections
+
+        dataset_axis = hist.Cat("dataset", "Primary dataset")
+        channel_axis = hist.Cat("channel", "Channel")
+
+        hist.Hist.DEFAULT_DTYPE = 'f'  # save some space by keeping float bin counts instead of double
+        self._accumulator = processor.dict_accumulator()
+
+        self._accumulator['cutflow'] = processor.defaultdict_accumulator(int)
+        self._accumulator['sumw'] = processor.defaultdict_accumulator(int)
+        
+
+    @property
+    def accumulator(self):
+        return self._accumulator
+
+
+    def process(self, df):
+        logging.debug('starting process')
+        output = self.accumulator.identity()
+
+        dataset = df['dataset']
+        self._isData = dataset in ['SingleMuon','DoubleMuon','SingleElectron','DoubleEG','EGamma','MuonEG']
+
+        selection = processor.PackedSelection()
+
+        output['cutflow']['all events'] += df['event'].size
+
+        logging.debug('applying lumi mask')
+        if self._isData:
+            lumiMask = lumi_tools.LumiMask(self._corrections['golden'])
+            df['passLumiMask'] = lumiMask(df['run'],df['luminosityBlock'])
+        else:
+            df['passLumiMask'] = np.ones_like(df['run'],dtype=bool)
+        passLumiMask = df['passLumiMask']
+        selection.add('lumiMask',passLumiMask)
+
+        output['cutflow']['golden json'] += sum(passLumiMask)
+
+        return output
+
+
+    def postprocess(self, accumulator):
+        # always scale to 1000 pb for plotting
+        lumi = 1000
+        scale = {}
+        for dataset, sumw in accumulator['sumw'].items():
+            if not sumw: continue
+            if dataset in self._corrections['xsec']:
+                scale[dataset] = lumi*self._corrections['xsec'][dataset]/sumw
+            else:
+                print(f'missing cross section for {dataset}')
+                scale[dataset] = lumi / sumw
+            
+        for h in accumulator.values():
+            if isinstance(h, hist.Hist):
+                h.scale(scale, axis="dataset")
+ 
+        return accumulator
+
+
+if __name__ == '__main__':
+
+    years = ['2016','2017','2018']
+    for year in years:
+        corrections = load(f'corrections/corrections_{year}.coffea')
+
+        processor_instance = TemplateProcessor(
+            year=year,
+            corrections=corrections,
+        )
+
+        save(processor_instance, f'processors/templateProcessor_{year}.coffea')
--- wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-tar -zxf columnar.tar.gz
-source columnar/bin/activate
-
-echo "Running command:" $@
-eval $@ || exit $?
